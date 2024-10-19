8dbec4e6425c2adf57ca4d6d0fc57c90bafe4968
Pankaj Singh <pankrj123@gmail.com>
2024-10-19 12:08:30 +0000

    randompassword generator

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -15,8 +15,7 @@ function App() {
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
             onClick={() => setColor("red")}
-            className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor:'red'}}
-          >
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor:'red'}}>
             Red
           </button>
           <button onClick={()=>setColor('green')} className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor:'greenyellow'}}>
